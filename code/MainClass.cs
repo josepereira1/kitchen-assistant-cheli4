763e66389920aaeaea0b70c670ed16948c3af4bb
RicardoPetronilho98 <rpetronilho98@gmail.com>
2019-04-05 22:03:16 +0100

    código de reconhecimento de voz começado

---
+++ code/MainClass.cs
@@ -0,0 +1,15 @@
+﻿namespace speech_hello_world
+{
+    class MainClass
+    {
+        public static void Main()
+        {
+            Reconhecimento rec = new Reconhecimento();
+
+            /* escuta o programador N vezes ao dizer a expressão "hey chely" 
+               e escreve no ficheiro o texto reconhecido */
+            int N = 5;
+            rec.LearnExpressions("hey_chely.txt", N);
+        }
+    }
+}
---
+++ code/Reconhecimento.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.CognitiveServices.Speech;
+
+namespace speech_hello_world
+{
+    class Reconhecimento
+    {
+        private String result; // variável privada para armazenamento do reconhecimento de voz
+
+        /// <summary>Construtor vazio. Cria o objeto Recognition útil para reconhecimento de voz.</summary>
+        public Reconhecimento()
+        {
+            this.result = "";
+        }
+
+        /// <summary>
+        /// Escuta uma ou mais expressões e devovle o resultado reconhecido em formato String.
+        /// </summary>
+        /// <returns>Devolve o reconhecimento de voz em formato String.</returns>
+        public String Listen()
+        {
+            this.result = ""; // reset à variável de classe
+            Console.WriteLine("listening..."); // apenas para no terminal saber que está à escuta
+            this.RecognizeSpeechAsync().Wait(); // efetua o reconhecimento de voz
+            Console.WriteLine("Listened => " + this.result);
+            return this.result; 
+        }
+
+        /// <summary>
+        /// Método utilizado para aprender novas expressões.
+        /// Exemplo de uso: learnExpressions("hey_chely.txt", 10) 
+        /// Pede ao programador para repetir 10 vezes a expressão "hey chelly" e guarda o texto
+        /// reconhecido no ficheiro. 
+        /// Desta forma o programador obtém as várias expressões, que podem
+        /// ser diferentes, mas que no entanto correspondem a "hey chely". 
+        /// Exmplo de output no ficheiro:
+        /// {"hey shelly", "hey shelly,", "hey shelly.", "hey kelly", "hello chelly", ...}
+        /// </summary>
+        /// <param name="fileName">nome do ficheiro.</param>
+        /// <param name="N">número de vezes pedido ao programador para ditar a expressão.</param>
+        public void LearnExpressions(String fileName, int N) 
+        {
+            StringBuilder sb = new StringBuilder(); 
+            for (int i=0; i<N; i++)
+            {
+                String str = this.Listen();
+                sb.Append(str).Append('\n');
+            }
+            this.writeOnFile(fileName, sb.ToString());
+        }
+
+        /// <summary>
+        /// Escreve a String no ficheiro.
+        /// </summary>
+        /// <param name="fileName">nome do ficheiro.</param>
+        /// <param name="str">String a ser escrita.</param>
+        private void writeOnFile(String fileName, String str) {
+            System.IO.File.WriteAllText(fileName, str);
+        }
+
+        /// <summary>
+        /// Ativa o reconhecimento de voz, ficando à escuta de uma ou mais expressões.
+        /// </summary>
+        /// <returns>Retorna a tarefa de reconhecimento de voz.</returns>
+        private async Task RecognizeSpeechAsync()
+        {
+            // Creates an instance of a speech config with specified subscription key and service region.
+            // Replace with your own subscription key and service region (e.g., "westus").
+            var config = SpeechConfig.FromSubscription("84499d48ad0646038b39623e46e12228", "westus");
+
+            // Creates a speech recognizer.
+            using (var recognizer = new SpeechRecognizer(config))
+            {
+                // Starts speech recognition, and returns after a single utterance is recognized. The end of a
+                // single utterance is determined by listening for silence at the end or until a maximum of 15
+                // seconds of audio is processed.  The task returns the recognition text as result. 
+                // Note: Since RecognizeOnceAsync() returns only a single utterance, it is suitable only for single
+                // shot recognition like command or query. 
+                // For long-running multi-utterance recognition, use StartContinuousRecognitionAsync() instead.
+                var result = await recognizer.RecognizeOnceAsync();
+
+                // Checks result.
+                if (result.Reason == ResultReason.RecognizedSpeech)
+                {
+                    String text = result.Text;
+                    this.result = text.ToLower();
+                }
+                else if (result.Reason == ResultReason.NoMatch)
+                {
+                    Console.WriteLine($"NOMATCH: Speech could not be recognized.");
+                }
+                else if (result.Reason == ResultReason.Canceled)
+                {
+                    var cancellation = CancellationDetails.FromResult(result);
+                    Console.WriteLine($"CANCELED: Reason={cancellation.Reason}");
+
+                    if (cancellation.Reason == CancellationReason.Error)
+                    {
+                        Console.WriteLine($"CANCELED: ErrorCode={cancellation.ErrorCode}");
+                        Console.WriteLine($"CANCELED: ErrorDetails={cancellation.ErrorDetails}");
+                        Console.WriteLine($"CANCELED: Did you update the subscription info?");
+                    }
+                }
+            }
+        }
+    }
+}
