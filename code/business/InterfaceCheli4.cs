4e3e276140810373ce01a208d3cfbad83a68e8c6
josepereira1 <joseandremp1@gmail.com>
2019-04-06 17:02:39 +0100

    interface Utilizador e Cheli4

---
+++ code/business/InterfaceCheli4.cs
@@ -0,0 +1,4 @@
+interface InterfaceCheli4{
+	public boolean GetLocalizacao();
+	public List<string> getSuperMercados(string localizacao);
+}
---
+++ code/business/RecursosHumanos/InterfaceUtilizador.cs
@@ -0,0 +1,5 @@
+using System.Collections.Generic;
+
+interface InterfaceUtilizador{
+	List<string> GetPreferencias();
+}
