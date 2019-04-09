c181e91dbf20f8610c87de449af0e0eabc73b08a
josepereira1 <joseandremp1@gmail.com>
2019-04-09 18:19:47 +0100

    mudei o nome dos ficheiros das interfaces, para a convenção em c#

--- 3-fase/cheli4/cheli4/Models/Comercial/Passo.cs
+++ 3-fase/cheli4/cheli4/Models/Comercial/Passo.cs
@@ -1,6 +1,12 @@
-﻿namespace cheli4.Models.Comercial
+﻿using System.Collections.Generic;
+namespace cheli4.Models.Comercial
 {
     public class Passo
     {
+        public string id;
+        public string descricao;
+        public Dictionary<string,string> expressoes;
+        public int duracao; // em millisegundos, para facilitar a
+
     }
 }
