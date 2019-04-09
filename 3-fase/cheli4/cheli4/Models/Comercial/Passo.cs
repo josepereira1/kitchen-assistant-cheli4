f612ad05559213a4188c9c9542fa71b39042bcb7
josepereira1 <joseandremp1@gmail.com>
2019-04-09 18:27:04 +0100

    nada demais

--- 3-fase/cheli4/cheli4/Models/Comercial/Passo.cs
+++ 3-fase/cheli4/cheli4/Models/Comercial/Passo.cs
@@ -3,10 +3,13 @@ namespace cheli4.Models.Comercial
 {
     public class Passo
     {
+        /*
+        //  este código não está terminado!!!
         public string id;
         public string descricao;
         public Dictionary<string,string> expressoes;
         public int duracao; // em millisegundos, para facilitar a
+        */
 
     }
 }
